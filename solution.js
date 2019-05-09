ffb76745545470995da146b9700fd3039a0d1231
xiangzhengmao <xiangzhengmao@berkeley.edu>
2019-05-09 22:00:36 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,7 +1,6 @@
 {
     init: function(elevators, floors) {
-        var i;
-        for (i=0;i<floors.length;i++){
+        for (var i=0;i<floors.length;i++){
             (function () {
                 var f = floors[i];
                 f.upReq = false;
@@ -43,26 +42,15 @@
         }
     },
     update: function(dt, elevators, floors) {
-        var i;
-        for (i=0; i<elevators.length; i++){
-            var e=elevators[i];
-            if (e.task>=0 && !floors[e.task].buttonStates.up && !floors[e.task].buttonStates.down){
-                if (!e.getPressedFloors().includes(e.task)) {
-                    e.destinationQueue = e.destinationQueue.filter(function(ele){return ele != e.task;});
-                    e.checkDestinationQueue();
-                }
-                e.task=-1;
-            }
-            if (e.loadFactor()==0.0 && e.task==-1) {
-                var j;
-                for (j=floors.length-1;j>0;j--){
-                    if (floors[j].upReq || floors[j].downReq){break;}
-                }
+        for (var i=0; i<elevators.length; i++){
+            if (elevators[i].task>=0 && !floors[elevators[i].task].buttonStates.up && !floors[elevators[i].task].buttonStates.down){elevators[i].task=-1;}
+            if (elevators[i].loadFactor()==0.0 && elevators[i].task==-1) {
+                for (var j=floors.length-1;j>0;j--) {if (floors[j].upReq || floors[j].downReq) {break;}}
                 floors[j].upReq = false;
                 floors[j].downReq = false;
-                e.task = j;
-                e.stop();
-                e.goToFloor(j);
+                elevators[i].task = j;
+                elevators[i].stop();
+                elevators[i].goToFloor(j);
             }
         }
     }
